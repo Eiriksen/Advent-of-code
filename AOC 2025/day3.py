e08137ec019f176a5b19f1b7cdb2ef5a2870a96e
Eirik Ryvoll Åsheim <eirik.ryvoll.asheim@slu.se>
2025-12-04 15:28:08 +0100

    Clean up day 3 code

--- AOC 2025/day3.py
+++ AOC 2025/day3.py
@@ -1,16 +1,18 @@
-# Day 1, turning on batteries, or finding the largest n-digit number you can make from a string of numbers, picking the numbers in the order they appear
-# Task 1: finding the largest 2-digit numbers and summing them
+# Day 3, turning on batteries, or finding the largest n-digit number you can make from a string of numbers, picking the numbers in the order they appear
 
-def locate_in_list(li,x):
-   return([i for i, z in enumerate(li) if z==x])
+
+### Task 1: finding the largest 2-digit numbers and summing them ----------------
+
+def locate_in_list(li,symbol):
+   return([i for i, x in enumerate(li) if x == symbol])
 
 def get_2batteries(bank):
   bank = [int(i) for i in bank]
-  block1 = bank[0:len(bank)-1]
-  b1 = max(block1)
-  pos1 = locate_in_list(block1,b1)[0]
-  block2 = bank[pos1+1:len(bank)]
-  b2 = max(block2)
+  subbank1 = bank[0:len(bank)-1]
+  b1 = max(subbank1)
+  pos1 = locate_in_list(subbank1,b1)[0]
+  subbank2 = bank[pos1+1:len(bank)]
+  b2 = max(subbank2)
   return(10*b1+1*b2)
 
 input = open("AOC 2025/input day 3.txt","r").read().splitlines()
@@ -19,32 +21,34 @@ input = open("AOC 2025/input day 3.txt","r").read().splitlines()
 sum([get_2batteries(i) for i in input])
 
 
-# Task 2: finding the largest 12-digit numbers and summing them
+### Task 2: finding the largest 12-digit numbers and summing them ----------------
 
 def collapse_to_int(li):
+  # collapses a list of digis to a single integer
   string = ""
   for i in li:
     string = string + str(i)
   return(int(string))
 
-def get_nth_battery(block,nrest):
-  block = [int(i) for i in block]
-  subblock = block[0:len(block)-nrest]
-  battery = max(subblock)
-  pos = locate_in_list(subblock,battery)[0]
-  restblock = block[pos+1:len(block)]
-  return([battery,restblock])
+def get_nth_battery(subbank,nrest):
+  # takes a subbank, and finds the highest digit while holding off some number of digits at the end of the subbank (nrest), and returns 1) that highest digit and 2) the remaining subbank (in a list)
+  block = [int(i) for i in subbank]
+  subbank2 = block[0:len(block)-nrest]
+  battery = max(subbank2)
+  pos = locate_in_list(subbank2,battery)[0]
+  subbank3 = block[pos+1:len(block)]
+  return([battery,subbank3])
 
 def get_nbatteries(bank,n):
-  curbank = bank
+  subbank = bank
   batteries = []
   for i in range(1,n+1):
     # nth battery
     nrest = n-i
-    search = get_nth_battery(curbank,nrest)
+    search = get_nth_battery(subbank,nrest)
     batteries.append(search[0])
-    curbank = search[1]
+    subbank = search[1]
   return(collapse_to_int(batteries))
 
 # RESULT Task 2
-sum([get_nbatteries(i,12) for i in input])
+sum([get_nbatteries(bank,12) for bank in input])
